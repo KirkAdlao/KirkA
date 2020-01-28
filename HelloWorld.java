0d3a82881ad881aa77274b7e4bbb294e6f5ae33f
cstuser <kirkadlao@gmail.com>
2020-01-28 12:07:02 -0500

    initial ass

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,4 +5,20 @@ public class HelloWorld {
         System.out.println("Hello, World");
     }
 
-}
+}    
+/* Hello vanier 
+
+*/
+
+final double PI= 3.14159;
+
+double radius; 
+double areas; 
+
+radius = 3.5;
+
+area =  PI * radius * radius; 
+
+System.out.println ("The area is " + area) ;
+
+
